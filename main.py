0114bb9f95415a8df28176dff2870511fbadde93
Emmanuel J. Eppinger <ej.eppinger@gmail.com>
2019-09-07 08:23:13 -0400

    adds blink detection

--- main.py
+++ main.py
@@ -51,6 +51,22 @@ def check_task(task_id, ref_pic, user_pics):
 
     return False
 
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+
+	# return the eye aspect ratio
+	return ear
+
 def user_blinked(user_pics):
     COUNTER = 0
     TOTAL = 0
@@ -61,7 +77,7 @@ def user_blinked(user_pics):
     frame = cv2.imread(user_pics[0].name)
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter("temp.mp4", 0, 1, (width,height))
+    video = cv2.VideoWriter("temp.avi", 0, 1, (width,height))
 
     for user_pic in user_pics:
         video.write(cv2.imread(user_pic.name))
@@ -69,14 +85,12 @@ def user_blinked(user_pics):
     cv2.destroyAllWindows()
     video.release()
 
-    vs = FileVideoStream("temp.mp4").start()
+    vs = FileVideoStream("temp.avi").start()
 
     for _ in user_pics:
         try:
-            print("here")
             frame = vs.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            print("here")
 
             rects = detector(gray, 0)
 
@@ -107,6 +121,7 @@ def user_blinked(user_pics):
             continue
 
 
+    print("total blinks:", TOTAL)
     # return TOTAL > 0
     return True
 
