ccc2bd125722704614c080aa68551d15cbeb6272
Aneek🙈 <aneekm@andrew.cmu.edu>
2019-09-07 13:57:32 -0400

    randomly picks key instead of int

--- task_assigner.py
+++ task_assigner.py
@@ -1,5 +1,5 @@
 import json
-from random import randint
+from random import choice
 from tasks import *
 
 def respond(resp, err):
@@ -13,7 +13,7 @@ def respond(resp, err):
 
 def lambda_handler(event, context):
     if event['httpMethod'] == 'GET':
-        taskId = randint(1, numTasks)
+        taskId = choice(list(task_arg_dict))
 
         return respond(
             {'description': task_arg_dict[taskId]},
