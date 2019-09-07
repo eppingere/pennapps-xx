1c10a3cc169d44f3d35ed66289ac6a36dbfcb1bd
Conlon Novak <conlon.nov@gmail.com>
2019-09-07 14:05:52 -0400

    File structure changes

--- flask_demo_site3/app.py
+++ flask_demo_site3/app.py
@@ -1,35 +1,44 @@
-from flask import Flask, render_template, request, Response, redirect
+from flask import Flask, render_template, request, Response, redirect, url_for
 
-# import boto3
+import boto3
 # from boto3.dynamodb.conditions import Key, Attr
 import json
 
-#TO-DO:
-# 1. Solve the error with 'WAIT' not being a valid field in the dynamodb
-# 2. Make sure that we can change the wait times with the three that we showed
-# 3. Stand the thing up in S3 
-# 4. Fix the URL_FOR error that we keep getting: 
+import base64, requests
+from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 
+auth = BotoAWSRequestsAuth(aws_host='htozj1y344.execute-api.us-east-1.amazonaws.com',
+                    aws_region='us-east-1',
+                    aws_service='execute-api')
 
-# Helper class to convert a DynamoDB item to JSON.
-# class DecimalEncoder(json.JSONEncoder):
-#     def default(self, o):
-#         if isinstance(o, decimal.Decimal):
-#             if o % 1 > 0:
-#                 return float(o)
-#             else:
-#                 return int(o)
-#         return super(DecimalEncoder, self).default(o)
+user = None
+prompt = None
 
+# Create an S3 client
+s3 = boto3.client('s3')
+
+filename = 'file.txt'
+bucket_name = 'my-bucket'
 
 app = Flask(__name__)
+
+users = ["conlonn@andrew.cmu.edu", 'aneekm@andrew.cmu.edu','eyluo@andrew.cmu.edu','manny@cmu.edu']
+
 # dynamodb= boto3.resource('dynamodb')
 # insurers_table = dynamodb.Table('insurers')
 # hospitals_table = dynamodb.Table('hospitals')
+@app.route("/")
+def index(): 
+	user = None
+	prompt = None
+	return render_template('index.html')
+
 
 @app.route("/success")
 def success(): 
-	return render_template('welcome.html')
+	user = None
+	prompt = None
+	return render_template('success.html')
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -37,36 +46,55 @@ def login():
 	username = request.form.get("username")
 	print(username)
 	if checkusername(username):
-		#THIS SHOULD BE WHERE WE CHECK WITH ARioWare---------------------------------------------------
-		# response = hospitals_table.query(
-  #  		KeyConditionExpression=Key('name').eq(username)
-		# )
-		# info = response['Items'][0]
-		# print(info)
-		# ##want to get address, phone number, wait time
-		return render_template("2fa.html", username=username)
+		user = username
+		return redirect(url_for('authenticate'))
 	return render_template('login.html')
 
-@app.route("/2fa", methods=["GET", "POST"])
-def twoFactor(username):
-	if twoFactorAuthenticate(username):
-		#THIS SHOULD BE WHERE WE CHECK WITH ARioWare---------------------------------------------------
-		# response = hospitals_table.query(
-  #  		KeyConditionExpression=Key('name').eq(username)
-		# )
-		# info = response['Items'][0]
-		# print(info)
-		# ##want to get address, phone number, wait time
-		return render_template('welcome.html')
+@app.route("/authenticate/", methods=["GET", "POST"])
+def authenticate():
+	if twoFactorAuthenticate(user):
+		return render_template('success.html')
 	return render_template('login.html')
 
 def checkusername(username): 
-	#username = request.form.get("username")
-	if username in ["conlonn@andrew.cmu.edu", 'aneekm@andrew.cmu.edu','eyluo@andrew.cmu.edu','manny@cmu.edu']:
+	if username in users:
 		return True
 	else: 
-		print("doesnt work")
+		print("Username not in \"database.\"")
 		return False
 
 def twoFactorAuthenticate(username):
 	return True
+	#same face?
+	#did they blink?
+	# task_id, obj_label, obj_qual = get_task()
+	# upload_photo_s3(pic)
+	# return check_task()
+
+def get_task(): #TO-DO? Mostly done?
+	response = requests.get(
+        'https://htozj1y344.execute-api.us-east-1.amazonaws.com/default/get-task',
+        auth=auth)
+
+	print(response.text)
+    #dictionary of ID, obj label, obj quality
+    #build prompt and display it
+
+def upload_photo_s3(pic): #TO-DO
+	# Uploads the given file using a managed uploader, which will split up large
+	# files automatically and upload parts in parallel.
+	s3.upload_file(filename, bucket_name, filename)
+
+
+def check_task(): #TO-DO
+	response = requests.post(
+        'https://htozj1y344.execute-api.us-east-1.amazonaws.com/default/get-task',
+        auth=auth)
+
+def register(): #TO-DO
+	#username
+	#password
+	#reference picture
+	return None
+
+#how are we adding new users to s3
