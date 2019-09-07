875c47bae890c80b1255d0a8088ccd5229872e7e
Eugene Luo <eyluo@andrew.cmu.edu>
2019-09-07 19:47:23 -0400

    added webcame functionity

--- flask_demo_site3/app.py
+++ flask_demo_site3/app.py
@@ -1,4 +1,10 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from flask import Flask, render_template, request, Response, redirect, url_for
+from task_assigner import respond, lambda_handler
+from tasks import *
+import json
 
 import boto3
 # from boto3.dynamodb.conditions import Key, Attr
@@ -22,20 +28,20 @@ bucket_name = 'my-bucket'
 
 app = Flask(__name__)
 
-users = ["conlonn@andrew.cmu.edu", 'aneekm@andrew.cmu.edu','eyluo@andrew.cmu.edu','manny@cmu.edu']
-
 # dynamodb= boto3.resource('dynamodb')
 # insurers_table = dynamodb.Table('insurers')
 # hospitals_table = dynamodb.Table('hospitals')
 @app.route("/")
-def index(): 
+def index():
+	global user, prompt
 	user = None
 	prompt = None
 	return render_template('index.html')
 
 
 @app.route("/success")
-def success(): 
+def success():
+	global user, prompt
 	user = None
 	prompt = None
 	return render_template('success.html')
@@ -43,20 +49,28 @@ def success():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+	global user, prompt
 	username = request.form.get("username")
 	print(username)
-	if checkusername(username):
-		user = username
-		return redirect(url_for('authenticate'))
+	if checkUsername(username):
+		taskID, objectType, quality = getTaskInfo()
+		if (quality == "exist"):
+			prompt = 'a picture of you holding a %s' % (objectType.lower())
+		else:
+			prompt = 'a picture of you holding a %s on the %s side of your face' % (objectType.lower(), quality.lower())
+		return render_template('authenticate.html', prompt=prompt)
 	return render_template('login.html')
 
 @app.route("/authenticate/", methods=["GET", "POST"])
 def authenticate():
-	if twoFactorAuthenticate(user):
-		return render_template('success.html')
-	return render_template('login.html')
+	return render_template('authenticate.html')
+	# if twoFactorAuthenticate(user):
+	# 	return render_template('success.html')
+	# return render_template('login.html')
 
-def checkusername(username): 
+def checkUsername(username): 
+	users = ["conlonn@andrew.cmu.edu", 'aneekm@andrew.cmu.edu',
+    	'eyluo@andrew.cmu.edu', 'manny@cmu.edu']
 	if username in users:
 		return True
 	else: 
@@ -64,34 +78,42 @@ def checkusername(username):
 		return False
 
 def twoFactorAuthenticate(username):
-	return True
 	#same face?
 	#did they blink?
-	# task_id, obj_label, obj_qual = get_task()
-	# upload_photo_s3(pic)
-	# return check_task()
+	taskID, objectType, quality = getTaskInfo()
+	print(taskID, objectType, quality)
+	# prompt = "Take a picture of yourself holding a %s in your %s hand" % ()
+	# Prompt user to take photo on webcam
+	# uploadPhotoS3(pic)
+	# return checkTask(taskID, username, livePath)
+	return True
 
-def get_task(): #TO-DO? Mostly done?
-	response = requests.get(
+def getTaskInfo(): #TO-DO? Mostly done?
+	global auth
+	rawResponse = requests.get(
         'https://htozj1y344.execute-api.us-east-1.amazonaws.com/default/get-task',
         auth=auth)
 
-	print(response.text)
-    #dictionary of ID, obj label, obj quality
-    #build prompt and display it
+	if (rawResponse == None):
+		return None, None, None
 
-def upload_photo_s3(pic): #TO-DO
+	responseJSON = json.loads(rawResponse.text)['description']
+
+	return responseJSON['Id'], responseJSON['ObjectLabel'], responseJSON['ObjectQuality']
+
+def uploadPhotoS3(pic): #TO-DO
 	# Uploads the given file using a managed uploader, which will split up large
 	# files automatically and upload parts in parallel.
 	s3.upload_file(filename, bucket_name, filename)
 
 
-def check_task(): #TO-DO
+def checkTask(): #TO-DO
+	global auth
 	response = requests.post(
         'https://htozj1y344.execute-api.us-east-1.amazonaws.com/default/get-task',
         auth=auth)
 
-def register(): #TO-DO
+def registerNewUser(): #TO-DO
 	#username
 	#password
 	#reference picture
